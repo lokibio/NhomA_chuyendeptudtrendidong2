68143bbe732e4b97ceb6ef8230588a85266f4b32
waitingall <waitingal2@gmail.com>
2017-08-09 22:44:47 +0700

    Nhom A

---
+++ code/standard version 1.0/Travel/src/teamA/travel/SupermarketActivity.java
@@ -0,0 +1,14 @@
+package teamA.travel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SupermarketActivity extends Activity 
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.supermarket_layout);
+	}
+}
